165caaf05d4e973414620f1c5cd011ee88ccd728
DESKTOP-3940NLB\ninko <ninkoarielle@gmail.com>
2024-07-29 14:29:07 +0100

    feat: CRUD expense categories

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,5 @@
 <?php
 
-use App\Http\Controllers\BudgetController;
-use App\Http\Controllers\ExpenseCategoryController;
-use App\Http\Controllers\ExpenseController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
